cec29abf9aabc85e92b02ac3b9141cb906b8d13a
JuanmadelosH <juanmanuelgiraldoperez271@gmail.com>
2025-09-17 21:02:44 -0500

    Agrego funcionalidad de la Calculadora_IMC

--- app/src/main/java/com/example/proyecto_movil/Calculadora_IMC.java
+++ app/src/main/java/com/example/proyecto_movil/Calculadora_IMC.java
@@ -1,6 +1,12 @@
 package com.example.proyecto_movil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +16,54 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Calculadora_IMC extends AppCompatActivity {
 
+    private EditText et_altura,et_peso;
+    private TextView recomendacion;
+    private Button Info;
+    private String imcForm;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calculadora_imc);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        et_altura=(EditText) findViewById(R.id.etHeight);
+        et_peso=(EditText) findViewById(R.id.etWeight);
+        recomendacion =(TextView) findViewById(R.id.tvRecommendation);
+        Info=(Button) findViewById(R.id.btnMoreInfo);
+
+        Info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                masDetalle();
+            }
         });
+
+
+
+    }
+    public void Calcular(View view){
+        double altura = Double.parseDouble(et_altura.getText().toString());
+        double peso = Double.parseDouble(et_peso.getText().toString());
+        double imc =peso/(altura*altura);
+
+        imcForm = String.format("%.2f",imc);
+        if(imc < 18.5){
+            recomendacion.setText(imcForm +" Tienes un Peso Bajo");
+        } else if (imc>=18.5 && imc < 24.9) {
+            recomendacion.setText(imcForm +" Tienes un Peso Normal");
+        } else if (imc >= 24.9 && imc < 29.9) {
+            recomendacion.setText(imcForm +" Tienes Sobrepeso");
+        }else{
+            recomendacion.setText(imcForm +" Tienes Obesidad");
+        }
+
+    }
+    public void masDetalle(){
+        Intent intent = new Intent(Calculadora_IMC.this, appRecomendacion.class);
+        intent.putExtra("imc",imcForm);
+        startActivity(intent);
+
     }
 }
--- app/src/main/java/com/example/proyecto_movil/appRecomendacion.java
+++ app/src/main/java/com/example/proyecto_movil/appRecomendacion.java
@@ -1,6 +1,9 @@
 package com.example.proyecto_movil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class appRecomendacion extends AppCompatActivity {
 
+    private Button btn_alimentacion,btn_ejercicio;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_app_recomendacion);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btn_alimentacion =(Button) findViewById(R.id.btnAlimentacion);
+        btn_ejercicio=(Button) findViewById(R.id.btnEjercicio);
+
+        OnClickListener();
+
+    }
+    public void OnClickListener(){
+        btn_alimentacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(appRecomendacion.this, appAlimentacion.class);
+                startActivity(intent);
+            }
+        });
+        btn_ejercicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(appRecomendacion.this, appEjercicio.class);
+                startActivity(intent);
+            }
         });
     }
 }
